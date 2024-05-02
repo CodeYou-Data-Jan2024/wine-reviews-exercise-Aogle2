9c67f38db403035a0d484646051b20de0c213d9b
Aaron Ogle <aaronogle7@gmail.com>
2024-05-02 18:12:55 -0400

    Commiting something new

--- reviews.py
+++ reviews.py
@@ -1,22 +1,3 @@
-"""import pandas as pd
-
-datasource = r"data/winemag-data-130k-v2.csv.zip"
-
-main_df = pd.read_csv(datasource)
-empty = pd.DataFrame()
-
-
-print(main_df.columns)
-
-agg_df = main_df[['country','points']].groupby('country').count() #Creates a new DF of country and points, then isolates the unigue rows in country and then counti it.
-points_df = main_df[['country','points']].groupby('country').agg('mean').round(1) #Does the same as above but gets the mean and also rounds it by one decimal
-
-final_df = agg_df.rename(columns={'points':"count"}) #renames counts to points
-final_df['points'] = points_df['points']
-
-print(final_df)
-"""
-
 import pandas as pd
 
 datasource = r"data/winemag-data-130k-v2.csv.zip"
@@ -31,4 +12,4 @@ final_df = agg_df.rename(columns={'points':"count"}) #renames counts to points
 
 final_df['points'] = points_df['points']
 
-print(final_df.iloc[:7].to_csv("test.csv"))
+print(final_df.to_csv("data/WineReviews.csv"))
