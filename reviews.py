b3b4b5c2bdda5a7506d1f22d13a528f124165915
Aaron Ogle <aaronogle7@gmail.com>
2024-05-02 15:53:15 -0400

    testing deer god.

--- reviews.py
+++ reviews.py
@@ -2,29 +2,17 @@ import pandas as pd
 
 datasource = r"data/winemag-data-130k-v2.csv.zip"
 
-main_df = pd.read_csv(datasource,index_col=0)
-
-#print(main_df.columns())
-
-#print(new_df.columns)
-
-#print(main_df.country.value_counts())
-
-#print(main_df["points"] )
-
-country_values = main_df.country.iloc[6:].value_counts()
-country_points = main_df.points.iloc[6:].round(2).idxmax()
-
-new_df = pd.DataFrame(data={'country':[main_df.country],
-			'counts':[country_values],
-			'points':[country_points] })
-#print(country_values)
-#print(f"{country_points}")
-#new_df['country'] = main_df.country.iloc[6:]
-#new_df['counts'] = main_df.country.iloc[6:].value_counts()
-#new_df['points'] = main_df.points.iloc[6:].round(1).idxmax()
+main_df = pd.read_csv(datasource)
+empty = pd.DataFrame()
 
 
-print(new_df.columns)
-print(new_df.head())
-print(new_df)
+print(main_df.columns)
+
+agg_df = main_df[['country','points']].groupby('country').count() #Creates a new DF of country and points, then isolates the unigue rows in country and then counti it.
+points_df = main_df[['country','points']].groupby('country').agg('mean').round(1) #Does the same as above but gets the mean and also rounds it by one decimal
+
+final_df = agg_df.rename(columns={'points':"count"}) #renames counts to points
+final_df['points'] = points_df['points']
+
+print(final_df)
+
